21a84f4e5819be36541f72a28c7c06463c42ace3
njd80 <neil.donaldson04@gmail.com>
2021-02-01 10:51:53 +0000

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,34 @@
+/* global algoliasearch instantsearch */
+
+const searchClient = algoliasearch(
+  'UH8HNBNRQ2',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+);
+
+const search = instantsearch({
+  indexName: 'instant_search',
+  searchClient,
+});
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+  }),
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item: `
+<article>
+  <h1>{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</h1>
+  <p>{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}</p>
+  <p>{{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}</p>
+</article>
+`,
+    },
+  }),
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+  }),
+]);
+
+search.start();
